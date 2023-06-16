5b3b026f2ca31c5bf04ce3047c49112f9ae927a9
silence <103921455+cisojoker@users.noreply.github.com>
2023-06-16 09:23:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>AB GAAND PHATEGI NA BHAI</h2>
+      <h2>let us start </h2>
     </div>
   );
 }
